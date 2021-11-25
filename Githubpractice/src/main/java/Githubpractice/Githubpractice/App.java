083df6a9ca5463a87056dfc2d1e099f1d75524d2
Prabhas6 <seleniumdev20@gmail.com>
2021-11-25 18:25:44 +0530

    GitEcilipsecreation

---
+++ Githubpractice/src/main/java/Githubpractice/Githubpractice/App.java
@@ -0,0 +1,13 @@
+package Githubpractice.Githubpractice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
